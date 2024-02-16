959a3f209d6078ae7c985f1cb1d582cea9dca684
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-02-16 00:30:59 -0300

    Countdown clock working like pomodoros

--- App.js
+++ App.js
@@ -1,11 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, Button, StyleSheet, Text, View, Alert, Title } from 'react-native';
+import { useState } from 'react';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+
+
+
+const UrgeWithPleasureComponent = ( () => {
+  let pomodoroTime = 5;
+  let restTime=3;
+  const [key, setKey] = useState(0);
+  const [isPlayingR, setIsPlayingR] = useState(0);
+  const [durationR, setDurationR] = useState(pomodoroTime);
+  const [buttonTitle, setButtonTitle] = useState("Iniciar");
+  const [pomodorosDone, setPomodorosDone] = useState(0);
+  const [isPomodoroTime, setIsPomodoroTime] = useState(true);
+  return (
+    <View>
+      <CountdownCircleTimer
+        key={key}
+        size={300}
+        strokeWidth={25}
+        isPlaying={isPlayingR}
+        duration={durationR}
+        //initialRemainingTime={durationR}
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+        onComplete={() => {
+          if (isPomodoroTime==true) {
+            setPomodorosDone(pomodorosDone+1)
+            setIsPomodoroTime(false)
+            setDurationR(restTime)
+          } else {
+            setIsPomodoroTime(true)
+            setDurationR(pomodoroTime)
+          }
+          setKey(prevKey => prevKey + 1)
+          //(isPomodoroTime) ?  : 
+          return { 
+            shouldRepeat: true, 
+            delay: 1.5,
+          } 
+        }}
+      >
+        {({ remainingTime }) => <View>
+            <Text style={styles.titleText}>{returnSecondToClock(remainingTime)}</Text>
+            <Text>{remainingTime} / total {pomodorosDone} / dur: {durationR}</Text>
+            </View>
+        }
+      </CountdownCircleTimer>
+      <Button title={buttonTitle} onPress={()=>{
+        setDurationR(pomodoroTime)
+        newInitialRemainingTime=100;
+        if (isPlayingR==true) {
+          setKey(prevKey => prevKey + 1)
+          setIsPlayingR(false)
+          setButtonTitle("Iniciar")
+        } else {
+          setIsPlayingR(true)
+          setButtonTitle("Parar");          
+        }
+      }}></Button>
+    </View>
+  )
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
+      <UrgeWithPleasureComponent />
     </View>
   );
 }
@@ -13,8 +77,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DDD',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  countdownCircleTimer: {
+    width:"1000px",
+    backgroundColor: "#09d",
+  },
+  titleText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
 });
+
+
+const returnSecondToClock = function (totalSeconds) {
+  if (totalSeconds) {
+    let hours   = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+    let seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+    seconds = Math.round(seconds * 100) / 100; 
+
+    let result = "";//hide hours (hours < 10 ? "0" + hours : hours);
+    result += (minutes < 10 ? "0" + minutes : minutes);
+    result += ":" + (seconds  < 10 ? "0" + seconds : seconds);
+    return result;
+
+  }
+  return totalSeconds;
+};
