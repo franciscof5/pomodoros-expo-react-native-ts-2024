c9dd2c4a7f2e621d8837289f5b6817ac840f8188
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-02-18 02:08:40 -0300

    Add touchable over the screen, increase size of clock

--- App.js
+++ App.js
@@ -1,63 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { React, Button, StyleSheet, Text, View, Image, Alert, Title } from 'react-native';
+import { React, Button, StyleSheet, Text, View, Image, TouchableOpacity, Alert, Title } from 'react-native';
 import { useState } from 'react';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 const foca = require("./images/mascote_foca.png")
 
+
+
 const UrgeWithPleasureComponent = ( () => {
-  let pomodoroTime = 5;
-  let restTime=3;
+  let pomodoroTime = 2;
+  let restTime=1;
   const [key, setKey] = useState(0);
   const [isPlayingR, setIsPlayingR] = useState(0);
   const [durationR, setDurationR] = useState(pomodoroTime);
   const [buttonTitle, setButtonTitle] = useState("Iniciar");
   const [pomodorosDone, setPomodorosDone] = useState(0);
   const [isPomodoroTime, setIsPomodoroTime] = useState(true);
+  
+  const completeCicle = function() {
+    if (isPomodoroTime==true) {
+      //it just completed a pomodoro
+      setPomodorosDone(pomodorosDone+1)
+      setIsPomodoroTime(false)
+      setDurationR(restTime)
+    } else {
+      //it just finished a rest
+      setIsPomodoroTime(true)
+      setDurationR(pomodoroTime)
+    }
+    //always has to setKey
+    setKey(prevKey => prevKey + 1)
+    return { 
+      shouldRepeat: true, 
+      delay: 1.5,
+    } 
+  }
+
+  const actioButton = function() {
+    setDurationR(pomodoroTime)
+    if (isPlayingR==true) {
+      setKey(prevKey => prevKey + 1)
+      setIsPlayingR(false)
+      setButtonTitle("Iniciar")
+    } else {
+      setIsPlayingR(true)
+      setButtonTitle("Parar");          
+    }
+  }
+
   return (
     <View>
+      <TouchableOpacity style={styles.buttonFloat} onPress={()=>{actioButton()}} />
       <CountdownCircleTimer
         key={key}
         size={300}
         strokeWidth={25}
         isPlaying={isPlayingR}
         duration={durationR}
-        //initialRemainingTime={durationR}
         colors={['#004777', '#F7B801', '#A30000', '#A30000']}
         colorsTime={[7, 5, 2, 0]}
-        onComplete={() => {
-          if (isPomodoroTime==true) {
-            setPomodorosDone(pomodorosDone+1)
-            setIsPomodoroTime(false)
-            setDurationR(restTime)
-          } else {
-            setIsPomodoroTime(true)
-            setDurationR(pomodoroTime)
-          }
-          setKey(prevKey => prevKey + 1)
-          //(isPomodoroTime) ?  : 
-          return { 
-            shouldRepeat: true, 
-            delay: 1.5,
-          } 
-        }}
+        onComplete={ () => {
+          completeCicle()
+        } }
       >
         {({ remainingTime }) => <View>
             <Text style={styles.titleText}>{returnSecondToClock(remainingTime)}</Text>
-            <Text>{remainingTime} / total {pomodorosDone} / dur: {durationR}</Text>
+            {/* <Text>seconds: {durationR}</Text> */}
             </View>
         }
       </CountdownCircleTimer>
-      <Button title={buttonTitle} onPress={()=>{
-        setDurationR(pomodoroTime)
-        if (isPlayingR==true) {
-          setKey(prevKey => prevKey + 1)
-          setIsPlayingR(false)
-          setButtonTitle("Iniciar")
-        } else {
-          setIsPlayingR(true)
-          setButtonTitle("Parar");          
-        }
-      }}></Button>
+      <View style={styles.mascotView}>
+        <Image source={foca} style={styles.mascotImage}/>
+        <Text style={styles.mascotText}>{buttonTitle} / pomodoros done {pomodorosDone}  </Text>
+      </View>
     </View>
   )
 })
@@ -68,7 +82,6 @@ export default function App() {
       {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
       <UrgeWithPleasureComponent />
-      <Image source={foca} style={styles.imageLogo}/>
     </View>
   );
 }
@@ -84,15 +97,35 @@ const styles = StyleSheet.create({
     width:"1000px",
     backgroundColor: "#09d",
   },
+  buttonFloat: {
+    position: "absolute",
+    zIndex: 10,
+    width: 500,
+    height: 300,
+    backgroundColor: "transparent",
+  },
+  // mascotView: {
+  //   flex:1,
+  //   flexDirection: "row", 
+  // },
+  mascotImage: {
+    width: "100px",
+    height: "100px",
+  },
+  mascotText: {
+    padding:"10px",
+    margin:"10px",
+    width:"100px",
+    height:"100px",
+    borderRadius:5,
+    backgroundColor:"#EEE",
+  },
   titleText: {
     fontSize: 50,
     fontWeight: 'bold',
   },
-  imageLogo: {
-    width: "100px",
-    height: "100px",
-  },
 });
+
 
 
 const returnSecondToClock = function (totalSeconds) {
